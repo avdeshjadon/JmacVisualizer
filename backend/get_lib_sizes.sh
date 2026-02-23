3c5e14c0916e0f8e7b685dbe908f6cea5d8fc2da
avdeshjadon <theavdeshjadon@gmail.com>
2026-02-23 12:53:13 +0530

    chore: Remove testing/research scripts from backend

--- backend/get_lib_sizes.sh
+++
@@ -1,4 +0,0 @@
-for d in ~/Library/*; do
-  size=$(osascript -e "tell app \"Finder\" to size of (POSIX file \"$d\")")
-  echo "$d Finder: $size"
-done
--- backend/get_sizes.sh
+++
@@ -1,4 +0,0 @@
-for d in ~/Library ~/Documents ~/Downloads ~/Pictures ~/Movies ~/Music ~/Desktop; do
-  size=$(osascript -e "tell app \"Finder\" to size of (POSIX file \"$d\")")
-  echo "$d Finder: $size"
-done
